6a0b54b506575ad53136741f5dc8c92993fa0957
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2025-01-13 02:14:40 +0600

    fix: add changes on useModel for fixing user schema unregistered issue

--- model/userModel.ts
+++ model/userModel.ts
@@ -1,23 +1,25 @@
-import {Model, model, models, Schema, Document} from "mongoose";
-
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface User extends Document {
-    _id: string;
-    name: string;
-    email: string;
-    password: string;
-    profileImage?: string;
-    createdAt: Date;
-    updatedAt: Date;
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+  profileImage?: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const userSchema = new Schema<User>({
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    profileImage: {type: String, required: false}
-}, {timestamps: true});
+const userSchema = new Schema<User>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    profileImage: { type: String, required: false },
+  },
+  { timestamps: true }
+);
 
-const User = models.User as Model<User> || model<User>("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
